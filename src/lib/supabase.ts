c38e9d114d45a1fba6ebbb3c54ec959a2bfbf678
Pietro-Hoffmann <pietrohoffmann0@gmail.com>
2025-10-19 02:35:00 -0300

    mudança variáveis de ambiente

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mccbckcdclofjmycjubk.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jY2Jja2NkY2xvZmpteWNqdWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MzE1NDgsImV4cCI6MjA3NjQwNzU0OH0.XzCsrHlG3N371JDtM6JVAYtuK6q-dT0dH-lEoFP0xl0';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
